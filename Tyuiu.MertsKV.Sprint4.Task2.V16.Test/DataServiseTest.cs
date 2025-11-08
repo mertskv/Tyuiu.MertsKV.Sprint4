6c4a1607e14d1bede9b9f6da6e69c29ddf5b6e30
Кристина Мерц <Tyuiu.mertskv@outlook.com>
2025-11-08 11:56:29 +0500

    Task2

---
+++ Tyuiu.MertsKV.Sprint4.Task2.V16.Test/DataServiseTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MertsKV.Sprint4.Task2.V16.Lib;
+namespace Tyuiu.MertsKV.Sprint4.Task2.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataServise ds = new DataServise();
+
+            int[] numsArray = { 2, 2, 3, 5, 6, 8, 6, 7, 3, 3 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 21;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
