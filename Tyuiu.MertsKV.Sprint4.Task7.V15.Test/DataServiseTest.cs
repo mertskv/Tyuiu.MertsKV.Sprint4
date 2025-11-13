a977c0df8f9d2f9825b6a5d20e54f3668e039b3d
Кристина Мерц <Tyuiu.mertskv@outlook.com>
2025-11-13 21:14:55 +0500

    Task7

---
+++ Tyuiu.MertsKV.Sprint4.Task7.V15.Test/DataServiseTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MertsKV.Sprint4.Task7.V15.Lib;
+namespace Tyuiu.MertsKV.Sprint4.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataServise ds = new DataServise();
+            int rows = 4;
+            int colums = 2;
+            int[,] ints = new int[rows, colums];
+            string str = "10293847";
+            int res = ds.Calculate(rows, colums, str);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MertsKV.Sprint4.Task7.V15/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MertsKV.Sprint4.Task7.V15.Lib;
+namespace Tyuiu.MertsKV.Sprint4.Task7.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            int colums = 2;
+            int[,] mtrx = new int[rows, colums];
+
+            string str = "10293847";
+
+            DataServise ds = new DataServise();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, colums, str);
+            Console.WriteLine("Количество нечетных элементов = " + res);
+            Console.ReadKey();
+
+        }
+    }
+}
